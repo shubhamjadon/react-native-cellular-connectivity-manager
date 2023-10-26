ddfa07259f23d7a9433556b7f8c37a14e9544f20
Aashish Tiwari <aashisht@geekyants.com>
2023-10-26 12:19:19 +0530

    feat: added airplane mode event listener

--- ios/CellularConnectivityManager.mm
+++ ios/CellularConnectivityManager.mm
@@ -4,11 +4,36 @@
 @implementation CellularConnectivityManager
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(isAirplaneEnabled:(RCTPromiseResolveBlock)resolve (RCTPromiseRejectBlock)reject){
+@interface CellularConnectivityManagerDelegate : NSObject <CTTelephonyNetworkInfoDelegate>
+
+- (void)dataServiceIdentifierDidChange:(NSString *)identifier;
+
+@end
+
+@property (nonatomic, strong) CellularConnectivityManagerDelegate *delegate;
+
+
+RCT_EXPORT_METHOD(isAirplaneEnabled:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
       NSDictionary<NSString *,NSString *> * radio = [[CTTelephonyNetworkInfo alloc] init].serviceCurrentRadioAccessTechnology;
-      bool isEnabled = radio == nil;
+      bool isEnabled = radio.count == 0;
       resolve([NSNumber numberWithBool:isEnabled]);
 }
 
+- (void)setDataServiceIdentifierDidChangeDelegate:(CellularConnectivityManagerDelegate *)delegate {
+    self.delegate = delegate;
+}
+
+@end
+
+@implementation CellularConnectivityManagerDelegate
+
+- (void)dataServiceIdentifierDidChange:(NSString *)identifier {
+  // Get the new radio access technology for the service.
+  CTRadioAccessTechnology RAT = [CTTelephonyNetworkInfo serviceCurrentRadioAccessTechnology:identifier];
+
+  // Send an event to React Native with the new RAT.
+  NSDictionary *eventData = @{@"RAT": CTGetRadioAccessTechnologyString(RAT)};
+  [RCTDeviceEventEmitter sendDeviceEventWithName:@"cellularConnectivityDidChange" body:eventData];
+}
 
 @end
